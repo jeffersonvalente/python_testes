d24005a83d986280b366a639199b55d5f61e1201
jeffersonvalente <jeffersonvalente1988@gmail.com>
2021-12-30 09:44:36 -0300

    exemplos de valores inteiros imutaveis

---
+++ valores_imutaveis.py
@@ -0,0 +1,33 @@
+Python 3.8.10 (default, Sep 28 2021, 16:10:42) 
+[GCC 9.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #valores imutaveis
+>>> #30 =
+>>> x =50
+>>> print(id(x))
+9790560
+>>> #valores imutaveis
+>>> #30
+>>> x =30
+>>> print(id(x))
+9789920
+>>> 
+>>> 
+>>> #valores imutaveis
+>>> x=30
+>>> x+= 20
+>>> print(x)
+50
+>>> print(id(x))
+9790560
+>>> 
+>>> 
+>>> 
